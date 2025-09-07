01b2340d3e442110fbd69bfb42ad11b62c782877
Avishka777 <a.rathnakumara777@gmail.com>
2025-09-07 15:20:19 +0530

    Remove Nodejs Backend Component

--- backend/routers/email.router.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const emailController = require("../controllers/email.controller");
-const authenticateUser = require("../middleware/authMiddleware");
-
-// Send email with user progress
-router.post("/send", authenticateUser, emailController.sendSessionEmail );
-
-module.exports = router;
